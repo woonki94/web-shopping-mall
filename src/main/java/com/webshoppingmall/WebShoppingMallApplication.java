52681e95c31652f035d4443768fb8353cb2468f7
Kim Woonki <woonki94@naver.com>
2022-04-13 23:11:56 +0900

    Initial Commit

---
+++ src/main/java/com/webshoppingmall/WebShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package com.webshoppingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webshoppingmall/WebShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
