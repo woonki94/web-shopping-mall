179fa4a2f28c46fc7255d82bc88687577aeda37c
Kim Woonki <woonki94@naver.com>
2022-04-14 13:02:15 +0900

    querydsl 사용 설정

--- src/main/java/com/webshoppingmall/repository/ItemRepository.java
+++ src/main/java/com/webshoppingmall/repository/ItemRepository.java
@@ -2,9 +2,14 @@ package com.webshoppingmall.repository;
 
 import com.webshoppingmall.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>{
-    Item findByItemName(String itemName);
+    List<Item> findByItemName(String itemName);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findItemDetail(@Param("itemDetail") String itemDetail);
 }
--- src/test/java/com/webshoppingmall/repository/ItemRepositoryTest.java
+++ src/test/java/com/webshoppingmall/repository/ItemRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.webshoppingmall.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.webshoppingmall.constant.ItemSellStatus;
 import com.webshoppingmall.entity.Item;
+import com.webshoppingmall.entity.QItem;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,6 +21,21 @@ import java.util.List;
 public class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
+    @PersistenceContext
+    EntityManager em;
+
+    public void createItemList(){
+        for(int i =1; i<=10; i++){
+            Item item = new Item();
+            item.setItemName("테스트 상품명"+i);
+            item.setPrice(1000 + (i*100));
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
 
     @Test
     public void 상품_저장_테스트(){
@@ -37,19 +57,42 @@ public class ItemRepositoryTest {
     @Test
     public void 상품명으로_찾기_테스트(){
         //given
-        Item item = new Item();
-        String itemName= "상품명";
-        item.setItemName(itemName);
-        item.setPrice(1000);
-        item.setItemDetail("테스트 상품");
-        item.setItemSellStatus(ItemSellStatus.SELL);
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now());
+        this.createItemList();
         //when
-        Item savedItem = itemRepository.save(item);
-        Item findItem = itemRepository.findByItemName(itemName);
+        List<Item> itemList = itemRepository.findByItemName("테스트 상품명1");
         //then
-        Assertions.assertThat(savedItem.getItemDetail()).isEqualTo(findItem.getItemDetail());
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 
+    @Test
+    public void 쿼리를_이용한_조회_테스트(){
+        //given
+        this.createItemList();
+        //when
+        List<Item> itemList = itemRepository.findItemDetail("테스트 상품 상세 설명");
+        //then
+        for(Item i : itemList){
+            System.out.println(i.toString());
+        }
+    }
+
+    @Test
+    public void Querydsl_조회_테스트(){
+        //given
+        this.createItemList();
+        //when
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = jpaQueryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%"+"테스트 상품 상세 설명"+ "%"))
+                .orderBy(qItem.price.desc());
+        List<Item> itemList = query.fetch();
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 }
