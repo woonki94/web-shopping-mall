c8ce7eff4a9ff3a5a68026dbfc004d4f4c0da15f
Kim Woonki <woonki94@naver.com>
2022-04-15 13:09:26 +0900

    member

--- src/main/java/com/webshoppingmall/controller/MemberController.java
+++ src/main/java/com/webshoppingmall/controller/MemberController.java
@@ -6,9 +6,11 @@ import com.webshoppingmall.repository.MemberRepository;
 import com.webshoppingmall.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(value = "/members")
@@ -16,12 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
-        return "member/memberForm";
+        return "members/memberForm";
     }
+
+    @PostMapping(value = "/new")
+    public String memberForm(MemberFormDto memberFormDto){
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/";
+    }
+
 
 
 }
