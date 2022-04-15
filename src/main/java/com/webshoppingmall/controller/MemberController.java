ee31e3fa6e7751c5bd818022dfd7aadd081b5f00
Kim Woonki <woonki94@naver.com>
2022-04-15 12:57:25 +0900

    Member

--- src/main/java/com/webshoppingmall/controller/MemberController.java
+++ src/main/java/com/webshoppingmall/controller/MemberController.java
@@ -1,4 +1,27 @@
 package com.webshoppingmall.controller;
 
+import com.webshoppingmall.dto.MemberFormDto;
+import com.webshoppingmall.entity.Member;
+import com.webshoppingmall.repository.MemberRepository;
+import com.webshoppingmall.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/members")
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+
 }
